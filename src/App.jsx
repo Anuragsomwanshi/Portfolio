00452c23568498706c1d1eb3d534eab60a744e95
Anuragsomwanshi <anuragsomwanshi11@gmail.com>
2025-02-28 19:11:39 +0530

    components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Home from './components/Home/Home'
+import Navbar from './components/Navbar/Navbar'
+import About from './components/About/About'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+
+      <Navbar/>
+      <Home/>
+      <About/>
+    </div>
   )
 }
 
