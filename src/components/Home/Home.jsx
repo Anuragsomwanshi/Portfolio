9e95d51e768bb0fb3071e12b7b9432be29302e3a
Anuragsomwanshi <anuragsomwanshi11@gmail.com>
2025-03-28 19:48:10 +0530

    add version1

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import './Home.css'
+import prof_img from '../../assets/image.jpg'
 
 const Home = () => {
   return (
-    <div> this  is Home page
+    <div className='Home'>
 
-      is this your home page
+      <div className="content">
+      <img className='img' src={prof_img} alt="" />
+      <h4>Hello</h4>
+      <h2>i am Anurag Somwanshi</h2>
+      <p> i am Software Engineer working on MERN stack and python Django technology</p>
+
+      </div>
+
+
+
     </div>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 import "./Navbar.css"
 const Navbar = () => {
   return (
-    <div className='nav' > e</div>
+    <div className='nav' >
+      <div className='logo'>Anurag</div>
+      <ul className='menu'> 
+      <li>Home</li>
+      <li>About</li>
+      <li>Services</li>
+      <li>Portfolio</li>
+      <li>Contact</li>
+      </ul>
+      <button>contact</button>
+    </div>
   )
 }
 
